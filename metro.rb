7db2d3ca34f379570839225cef6972211b2985ad
Mike Jones <mikethespike77@gmail.com>
2015-08-27 18:35:15 +0100

    basic functionality, sort of

--- metro.rb
+++ metro.rb
@@ -1,29 +1,31 @@
-lines{
+lines = {
+			'n' =>	["ts", "34th", "28th", "23rd", "us", "8th"],
+			
 
-			n: ["ts", "34th", "28th", "23rd", "us", "8th"],
+			'l' =>  ["8th", "6th", "us", "3rd", "1st"],
+				
 
-			l:["8th", "6th", "us", "3rd", "1st"],
-
-			s: ["gc", "33rd", "28th", "23rd", "us", "astor"]
-}
+			's' =>  ["gc", "33rd", "28th", "23rd", "us", "astor"],
+		
+};
 
 
 
 puts "The lines availible are as shown: "
-puts "Line N: " + n.join(', ')
-puts "Line L: " + l.join(', ')
-puts "Line Sixth: " + s.join(', ')
+puts "Line N: " + lines['n'].join(', ').to_s
+puts "Line L: " + lines['l'].join(', ').to_s
+puts "Line Sixth: " + lines['s'].join(', ').to_s
 
 puts "Which line would you like: (n) N line, (l) L line or (s) Sixth line?"
-line_on = gets
+on = gets
 
 puts "Which station would you like to get on?"
 station_on = gets
 
 puts "Which line will you get off at?"
-line_off = gets
+off = gets
 
 puts "Which station would you like to get off at?"
 station_off = gets
 
-trip calculator = 
+trip calculator = lines[line_on][on] - lines[line_off][off]
